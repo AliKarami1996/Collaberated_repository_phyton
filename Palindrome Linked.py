2834575d9bac96ec90a1d1a4665da98c29bcffb6
Ali Karami <149543167+AliKarami1996@users.noreply.github.com>
2023-12-21 18:21:52 -0500

    Palindrome Linked.py
    Signed-off-by: Ali Karami <149543167+AliKarami1996@users.noreply.github.com>

--- Palindrome Linked.py
+++ Palindrome Linked.py
@@ -1,34 +1,3 @@
-"""
-Title: 234 - Palindrome Linked List
-Category: MATH, Arrays
-Source: LeetCode
-Source Link: https://leetcode.com/problems/palindrome-linked-list/
-
-Problem Statement:
-    Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
-
-Constraints:
-    - The number of nodes in the list is in the range [1, 105].
-    - 0 <= Node.val <= 9
-
-Example 1:
-    Input: head = [1,2,2,1]
-    Output: true
-
-Example 2:
-    Input: n = 5
-    Output: false
-
-Hints:
-    - Enter some numbers, then numbers will be checked for palindrome.
-
-How it works?
-    - Gets some number from input, then puts them in the list.
-    - Reverse the list, and compare the elements with the original list.
-    - If elements are equal, returns True, if not returns False.
-"""
-
-
 class CheckLinearPalindrome:
     def __init__(self, node):
         self.node = node
